d9d90f301868eff6862acc089e54df493755e8cd
Maximiliano Stein <maxi_stein23@hotmail.com>
2024-11-06 21:47:09 -0300

    user schema modified

--- components/user/schema.js
+++ components/user/schema.js
@@ -1,45 +1,57 @@
-import mongoose from 'mongoose'
-import validate from 'mongoose-validator'
-import bcrypt from 'bcrypt'
+import mongoose from 'mongoose';
+import validate from 'mongoose-validator';
+import bcrypt from 'bcrypt';
 
-const Schema = mongoose.Schema
-const { ObjectId } = Schema.Types
-const emailValidator = validate({ validator: 'isEmail' })
+const Schema = mongoose.Schema;
+const { ObjectId } = Schema.Types;
+const emailValidator = validate({ validator: 'isEmail' });
 
-const governmentIdTypes = ['cuil', 'cuit', 'dni', 'lc', 'le', 'pas']
-
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    validate: emailValidator,
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true, lowercase: true, trim: true },
+    lastName: { type: String, required: true, lowercase: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: emailValidator,
+    },
+    //Password must have one lowercase, one uppercase, one number and at least 8 characters.
+    password: {
+      type: String,
+      required: true,
+      select: false,
+      minlength: 8,
+      match: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$/,
+    },
+    role: { type: ObjectId, ref: 'Role', required: true },
+    supervisor: { type: ObjectId, ref: 'User', default: null },
+    phone: { type: String, trim: true },
+    bornDate: { type: Date },
+    isActive: { type: Boolean, default: true },
+    position: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Position',
+      required: true,
+    },
   },
-  password: { type: String, required: true, select: false },
-  role: { type: ObjectId, ref: 'Role', required: true },
-  firstName: { type: String, required: true, lowercase: true, trim: true },
-  lastName: { type: String, required: true, lowercase: true, trim: true },
-  phone: { type: String, trim: true },
-  governmentId: {
-    type: { type: String, enum: governmentIdTypes },
-    number: { type: String, trim: true },
+  { timestamps: true },
+  { versionKey: false },
+);
+
+userSchema.method(
+  'checkPassword',
+  async function checkPassword(potentialPassword) {
+    if (!potentialPassword) {
+      return Promise.reject(new Error('Password is required'));
+    }
+
+    const isMatch = await bcrypt.compare(potentialPassword, this.password);
+
+    return { isOk: isMatch, isLocked: !this.isActive };
   },
-  bornDate: { type: Date },
-  isActive: { type: Boolean, default: true },
-})
+);
 
-userSchema.index({ 'governmentId.type': 1, 'governmentId.number': 1 }, { unique: true })
-
-userSchema.method('checkPassword', async function checkPassword(potentialPassword) {
-  if (!potentialPassword) {
-    return Promise.reject(new Error('Password is required'))
-  }
-
-  const isMatch = await bcrypt.compare(potentialPassword, this.password)
-
-  return { isOk: isMatch, isLocked: !this.isActive }
-})
-
-export default userSchema
+export default userSchema;
