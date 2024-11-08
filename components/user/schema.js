4a98178b7b3060191255bb056bc891790f408bda
maximiliano-stein <maxi_stein23@hotmail.com>
2024-11-08 17:11:10 -0300

    user schema name lowercase removed

--- components/user/schema.js
+++ components/user/schema.js
@@ -8,13 +8,12 @@ const emailValidator = validate({ validator: 'isEmail' });
 
 const userSchema = new Schema(
   {
-    firstName: { type: String, required: true, lowercase: true, trim: true },
-    lastName: { type: String, required: true, lowercase: true, trim: true },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
     email: {
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
       trim: true,
       validate: emailValidator,
     },
