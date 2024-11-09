b9a403f74e7e4bf351fea58f574497ff36138b87
maximiliano-stein <maxi_stein23@hotmail.com>
2024-11-09 11:48:48 -0300

    naming modifications

--- bin/baseApi.js
+++ bin/baseApi.js
@@ -1,28 +1,32 @@
 /* eslint-disable no-undef */
-import figlet from 'figlet'
-import pkg from '../package.json' with { type: 'json' }
-import program from '../index.js'
+import figlet from 'figlet';
+import pkg from '../package.json' with { type: 'json' };
+import program from '../index.js';
 
 function start() {
-  process.stdout.write('\n')
-  process.stdout.write(`${figlet.textSync(`Base API server`, { font: 'Ogre' })}\n`)
-  process.stdout.write('\n')
-  process.stdout.write(`Version: ${pkg.version}, Environment: ${process.env.NODE_ENV || 'default'}\n`)
-  process.stdout.write('\n')
-  process.stdout.write('\n')
+  process.stdout.write('\n');
+  process.stdout.write(
+    `${figlet.textSync(`Org Management API`, { font: 'Ogre' })}\n`,
+  );
+  process.stdout.write('\n');
+  process.stdout.write(
+    `Version: ${pkg.version}, Environment: ${process.env.NODE_ENV || 'default'}\n`,
+  );
+  process.stdout.write('\n');
+  process.stdout.write('\n');
 
   program
     .start()
     .then((result) => {
-      program.logger.info(`BaseApi server server started`)
-      program.logger.info(`Environment ${process.env.NODE_ENV || 'default'}`)
+      program.logger.info(`API started`);
+      program.logger.info(`Environment ${process.env.NODE_ENV || 'default'}`);
       if (result && result.url) {
-        program.logger.info(`Listening for HTTP requests at ${result.url}`)
+        program.logger.info(`Listening for HTTP requests at ${result.url}`);
       }
     })
     .catch((err) => {
-      program.logger.error(`Failed to start BaseApi server`, err)
-    })
+      program.logger.error(`Failed to start the API`, err);
+    });
 }
 
-start()
+start();
